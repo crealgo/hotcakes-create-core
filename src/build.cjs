bc8a0294dca2f6e8c503b9e4c0548bcaef5cf26b
Manas Chimpidi <manaschimpidi@gmail.com>
2023-08-20 23:14:14 -0500

    feat: switch json

--- src/build.cjs
+++ src/build.cjs
@@ -1,6 +1,16 @@
 const childProcess = require('child_process');
+const fs = require('fs');
+
+// clean
+if (fs.existsSync('lib')) {
+	fs.rmSync('lib', { recursive: true });
+}
+
+fs.mkdirSync('lib');
 
 childProcess.execSync('node src/build-editorconfig.cjs');
 childProcess.execSync('node src/build-eslint.cjs');
 childProcess.execSync('node src/build-releaseconfig.cjs');
 childProcess.execSync('node src/build-tsconfig.cjs');
+
+fs.writeFileSync('lib/index.cjs', 'console.log(\'Hello World\')');
