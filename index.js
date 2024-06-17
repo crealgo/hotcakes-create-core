4310c215f64eced9ac7543268538448388c8a53d
Manas Chimpidi <manaschimpidi@gmail.com>
2024-06-16 21:11:36 -0500

    perf: abstract functions

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
 #!/usr/bin/env node
 
-async function main() {
-	const [nodeExec, execPath, command, ...flags] = process.argv;
+import {hello} from './scripts/hello.js';
+import {setup} from './scripts/setup.js';
 
-	if (command === 'hello') {
-		console.log('Hello, 🌎 World!');
+const main = async () => {
+	const [nodeExec, execPath, command] = process.argv;
+
+	switch (command) {
+		case 'hello':
+			hello();
+			break;
+		case 'setup':
+			await setup();
+			break;
+		default:
+			throw new Error(`Unknown command: ${command ?? 'undefined'}`);
 	}
-
-	if (command === 'setup') {
-		const {checkbox} = await import('@inquirer/prompts');
-
-		const response = await checkbox({
-			message: 'What configs do you want to add to your project?',
-			choices: [
-				{
-					name: 'Git Ignore',
-					value: '.gitignore',
-					checked: true,
-				},
-				{
-					name: 'Typescript Config',
-					value: 'tsconfig.json',
-					checked: true,
-				},
-				{
-					name: 'Semantic Release Config',
-					value: '.releaserc.json',
-					checked: true,
-				},
-				{
-					name: 'Eslint Config',
-					value: '.eslintrc.json',
-					checked: true,
-				},
-				{
-					name: 'Editor Config',
-					value: '.editorconfig',
-					checked: true,
-				},
-			],
-		});
-
-		const fs = await import('fs/promises');
-		const path = await import('path');
-		const url = await import('url');
-
-		const from = path.resolve('.', process.cwd());
-
-		console.log('from', url.fileURLToPath(import.meta.resolve('@hotcakes/core')));
-	}
-}
+};
 
 void main();
